fb2d6597322e27e0231db8e09baaa14fd2bdff53
Mohamed Dhia Ghorbel <dhiaghorbel0@gmail.com>
2023-12-04 18:42:12 +0100

    homecard

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
 
           <Slider />
 
-          <SubsCard />
+           {/* <SubsCard /> */}
           <Title>
             <Text>Latest Emissions</Text>
           </Title>
